c9773d25fcbacc7da03aff3eaff1ed31ab95d007
Biswajit Roy <broy16431@gmail.com>
2023-10-20 22:01:19 +0530

    changes

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-
+const OpenAI=require("openai")
+const cors=require("cors")
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const apiKey = process.env.OPENAPI_SECRET_KEY;
+const openAI=new OpenAI({apiKey})
 
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
-app.get("/quotes", async (req, res) => {
-  const { keyword } = req.query;
+app.get("/generate", async (req, res) => {
+  const { keyword,type } = req.query;
   try {
-    const apiEndpoint =
-      "https://api.openai.com/v1/engines/text-davinci-002/completions";
-
-    const requestData = {
-      prompt: `Generate a motivation quotes related to ${keyword}`,
-      max_tokens: 100,
-      temperature: 0.7,
-      n: 1
-    };
-
-    const response = await axios.post(apiEndpoint, requestData, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        "Content-Type": "application/json", // Add this line
-      },
-    });
-    const generatedContent = response.data.choices[0].text;
-
-    res.send(generatedContent);
+    let requestData=await openAI.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: `Generate a ${type} related to ${keyword}` }],
+        max_tokens: 100,
+    })
+    res.send(requestData.choices[0].message.content);
   } catch (error) {
-    console.error("Error generating content:", error.response.data);
-    res.status(500).send({"error":error.response.data});
+    console.error("Error generating content:",error);
+    res.status(500).send({"error":error.message});
   }
 });
 
